bf62b396b371ec1cdc2d1af7cd802d3fce53a9fc
VMikulasek <mikulasekvitek@gmail.com>
2024-07-03 11:35:27 +0200

    feat: change architecture of SIMDOperations that user can manipulate more with register

--- src/simd/operations/simd_operations_avx_float.hpp
+++ src/simd/operations/simd_operations_avx_float.hpp
@@ -15,103 +15,49 @@ namespace simd
         using NumType = float;
         static const InstructionSet INS_SET = InstructionSet::AVX;
 
-        static __m256 op1Reg;
-        static __m256 op2Reg;
-        static __m256 resultReg;
-
     public:
         /**
-         * @brief Loads vector of floats to float vector register
-         * that represents operand 1
+         * @brief 256 b vector register, works as intermediate result,
+         * has to be materialized for the value
+         */
+        using AvxReg = __m256;
+
+        /**
+         * @brief Loads vector of floats to register which is returned
          * 
          * @param vector Vector of floats to load to register
+         * @return Register with loaded vector
          */
-        static void load_op1(SIMDVector<NumType, INS_SET> &vector);
+        static AvxReg load_vector(SIMDVector<NumType, INS_SET> &vector);
 
         /**
-         * @brief Loads vector of floats to float vector register
-         * that represents operand 2
-         * 
-         * @param vector Vector of floats to load to register
-         */
-        static void load_op2(SIMDVector<NumType, INS_SET> &vector);
-
-        /**
-         * @brief Loads 2 vectors of floats into 2 vector float registers
-         * that represent both operands
-         * 
-         * @param vector1 Vector of floats to load to register representing op1
-         * @param vector2 Vector of floats to load to register representing op2
-         */
-        static void load_ops(SIMDVector<NumType, INS_SET> &vector1, SIMDVector<NumType, INS_SET> &vector2);
-
-        /**
-         * @brief Stores result from register to memory and returns
+         * @brief Stores vector from register to memory and returns
          * wrapped in SIMDVector
          * 
-         * @return SIMDVector<float, InstructionSet::AVX> Content of result register
+         * @param register 
+         * @return SIMDVector<float, InstructionSet::AVX> Content of register
          */
-        static SIMDVector<NumType, INS_SET> get_result();
+        static SIMDVector<NumType, INS_SET> materialize_register(AvxReg &reg);
 
         /**
-         * @brief Adds op1Reg and op2Reg and stores result into resultReg
+         * @brief Adds vec1 and vec2 and returns result
          */
-        static void add();
+        static AvxReg add(AvxReg &vec1, AvxReg &vec2);
 
         /**
-         * @brief Adds op1Reg and resultReg and stores result into resultReg
+         * @brief Subtracts vec2 from vec1 and returns result
          */
-        static void add_op1_result();
+        static AvxReg sub(AvxReg &vec1, AvxReg &vec2);
 
         /**
-         * @brief Adds op2Reg and resultReg and stores result into resultReg
+         * @brief Multiplies vec1 with vec1 and returns result
          */
-        static void add_op2_result();
+        static AvxReg mul(AvxReg &vec1, AvxReg &vec2);
 
         /**
-         * @brief Subtracts op2Reg from op1Reg and stores result into resultReg
+         * @brief Divides vec1 by vec2 and returns result
          */
-        static void sub();
-
-        /**
-         * @brief Subtracts op1Reg from resultReg and stores result into resultReg
-         */
-        static void sub_result_op1();
-
-        /**
-         * @brief Subtracts op2Reg from resultReg and stores result into resultReg
-         */
-        static void sub_result_op2();
-
-        /**
-         * @brief Multiplies op1Reg with op2Reg and stores result into resultReg
-         */
-        static void mul();
-
-        /**
-         * @brief Multiplies op1Reg with resultReg and stores result into resultReg
-         */
-        static void mul_op1_result();
-
-        /**
-         * @brief Multiplies op2Reg with resultReg and stores result into resultReg
-         */
-        static void mul_op2_result();
-
-        /**
-         * @brief Divides op1Reg by op2Reg and stores result into resultReg
-         */
-        static void div();
-
-        /**
-         * @brief Divides resultReg by op1Reg and stores result into resultReg
-         */
-        static void div_result_op1();
-
-        /**
-         * @brief Divides resultReg by op2Reg and stores result into resultReg
-         */
-        static void div_result_op2();
+        static AvxReg div(AvxReg &vec1, AvxReg &vec2);
     };
 }
 
--- src/simd/operations/simd_operations_avx_float.inl
+++ src/simd/operations/simd_operations_avx_float.inl
@@ -7,91 +7,42 @@
 
 namespace simd
 {
-    inline void SIMDOperations<float, InstructionSet::AVX>::load_op1(
+    inline SIMDOperations<float, InstructionSet::AVX>::AvxReg SIMDOperations<float, InstructionSet::AVX>::load_vector(
         SIMDVector<NumType, INS_SET> &vector)
     {
-        op1Reg = _mm256_load_ps(vector.get_content());
+        return _mm256_load_ps(vector.get_content());
     }
 
-    inline void SIMDOperations<float, InstructionSet::AVX>::load_op2(
-        SIMDVector<NumType, INS_SET> &vector)
-    {
-        op2Reg = _mm256_load_ps(vector.get_content());
-    }
-
-    inline void SIMDOperations<float, InstructionSet::AVX>::load_ops(
-        SIMDVector<NumType, INS_SET> &vector1,
-        SIMDVector<NumType, INS_SET> &vector2)
-    {
-        op1Reg = _mm256_load_ps(vector1.get_content());
-        op2Reg = _mm256_load_ps(vector2.get_content());
-    }
-
-    inline SIMDVector<float, InstructionSet::AVX> SIMDOperations<float, InstructionSet::AVX>::get_result()
+    inline SIMDVector<float, InstructionSet::AVX> SIMDOperations<float, InstructionSet::AVX>::materialize_register(
+        AvxReg &reg)
     {
         SIMDVector<NumType, INS_SET> result;
-        _mm256_store_ps(result.vector, resultReg);
+        _mm256_store_ps(result.vector, reg);
         return result;
     }
 
-    inline void SIMDOperations<float, InstructionSet::AVX>::add()
+    inline SIMDOperations<float, InstructionSet::AVX>::AvxReg SIMDOperations<float, InstructionSet::AVX>::add(
+        AvxReg &vec1, AvxReg &vec2)
     {
-        resultReg = _mm256_add_ps(op1Reg, op2Reg);
+        return _mm256_add_ps(vec1, vec2);
     }
 
-    inline void SIMDOperations<float, InstructionSet::AVX>::add_op1_result()
+    inline SIMDOperations<float, InstructionSet::AVX>::AvxReg SIMDOperations<float, InstructionSet::AVX>::sub(
+        AvxReg &vec1, AvxReg &vec2)
     {
-        resultReg = _mm256_add_ps(op1Reg, resultReg);
+        return _mm256_sub_ps(vec1, vec2);
     }
 
-    inline void SIMDOperations<float, InstructionSet::AVX>::add_op2_result()
+    inline SIMDOperations<float, InstructionSet::AVX>::AvxReg SIMDOperations<float, InstructionSet::AVX>::mul(
+        AvxReg &vec1, AvxReg &vec2)
     {
-        resultReg = _mm256_add_ps(op2Reg, resultReg);
+        return _mm256_mul_ps(vec1, vec2);
     }
 
-    inline void SIMDOperations<float, InstructionSet::AVX>::sub()
+    inline SIMDOperations<float, InstructionSet::AVX>::AvxReg SIMDOperations<float, InstructionSet::AVX>::div(
+        AvxReg &vec1, AvxReg &vec2)
     {
-        resultReg = _mm256_sub_ps(op1Reg, op2Reg);
-    }
-
-    inline void SIMDOperations<float, InstructionSet::AVX>::sub_result_op1()
-    {
-        resultReg = _mm256_sub_ps(resultReg, op1Reg);
-    }
-
-    inline void SIMDOperations<float, InstructionSet::AVX>::sub_result_op2()
-    {
-        resultReg = _mm256_sub_ps(resultReg, op2Reg);
-    }
-
-    inline void SIMDOperations<float, InstructionSet::AVX>::mul()
-    {
-        resultReg = _mm256_mul_ps(op1Reg, op2Reg);
-    }
-
-    inline void SIMDOperations<float, InstructionSet::AVX>::mul_op1_result()
-    {
-        resultReg = _mm256_mul_ps(op1Reg, resultReg);
-    }
-
-    inline void SIMDOperations<float, InstructionSet::AVX>::mul_op2_result()
-    {
-        resultReg = _mm256_mul_ps(op2Reg, resultReg);
-    }
-
-    inline void SIMDOperations<float, InstructionSet::AVX>::div()
-    {
-        resultReg = _mm256_div_ps(op1Reg, op2Reg);
-    }
-
-    inline void SIMDOperations<float, InstructionSet::AVX>::div_result_op1()
-    {
-        resultReg = _mm256_div_ps(resultReg, op1Reg);
-    }
-
-    inline void SIMDOperations<float, InstructionSet::AVX>::div_result_op2()
-    {
-        resultReg = _mm256_div_ps(resultReg, op2Reg);
+        return _mm256_div_ps(vec1, vec2);
     }
 }
 
