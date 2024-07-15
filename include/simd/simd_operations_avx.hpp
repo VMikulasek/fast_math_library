fe8b9d239d80563274ba0d861cdb28b0b3bc72e2
VMikulasek <mikulasekvitek@gmail.com>
2024-07-15 23:39:41 +0200

    feat: add checks for SIMD units

--- include/simd/simd_operations_avx.hpp
+++ include/simd/simd_operations_avx.hpp
@@ -1,6 +1,8 @@
 #ifndef SIMD_OPERATIONS_AVX_HPP
 #define SIMD_OPERATIONS_AVX_HPP
 
+#if __AVX__
 #include "../../src/simd/operations/simd_operations_avx_float.hpp"
+#endif
 
 #endif // SIMD_OPERATIONS_AVX_HPP
--- include/simd/simd_vector_avx.hpp
+++ include/simd/simd_vector_avx.hpp
@@ -1,6 +1,8 @@
 #ifndef SIMD_VECTOR_AVX_HPP
 #define SIMD_VECTOR_AVX_HPP
 
+#if __AVX__
 #include "../../src/simd/vector/simd_vector_avx_float.hpp"
+#endif
 
 #endif // SIMD_VECTOR_AVX_HPP
