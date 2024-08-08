a0175b60d948728c741616562c1da01779082b5f
VMikulasek <mikulasekvitek@gmail.com>
2024-08-08 18:12:04 +0200

    feat: amend runtimechecks to be compatible also for MSVC

--- runtimechecks/check_avx512_support.cpp
+++ runtimechecks/check_avx512_support.cpp
@@ -1,16 +1,45 @@
+#include <stdio.h>
+
+#if defined(_MSC_VER)
+#include <intrin.h>
+
+void cpuid(int cpuInfo[4], int function_id) {
+    __cpuid(cpuInfo, function_id);
+}
+
+void cpuidex(int cpuInfo[4], int function_id, int subfunction_id) {
+    __cpuidex(cpuInfo, function_id, subfunction_id);
+}
+
+#elif defined(__GNUC__) || defined(__clang__)
 #include <cpuid.h>
+
+void cpuid(int cpuInfo[4], int function_id) {
+    __get_cpuid(function_id, (unsigned int*)&cpuInfo[0], (unsigned int*)&cpuInfo[1], (unsigned int*)&cpuInfo[2], (unsigned int*)&cpuInfo[3]);
+}
+
+void cpuidex(int cpuInfo[4], int function_id, int subfunction_id) {
+    __cpuid_count(function_id, subfunction_id, cpuInfo[0], cpuInfo[1], cpuInfo[2], cpuInfo[3]);
+}
+
+#else
+#error "Unsupported compiler"
+#endif
 
 int main()
 {
-    unsigned int eax, ebx, ecx, edx;
-    if (__get_cpuid_count(7, 0, &eax, &ebx, &ecx, &edx)) {
-        if ((ebx & bit_AVX512F) != 0)
-        {
-            // supports AVX2
-            return 1;
-        }
+    int cpuInfo[4];
+
+    cpuidex(cpuInfo, 7, 0);
+    int ebx = cpuInfo[1];
+    int avx512Bit = 1 << 16;
+
+    if ((ebx & avx512Bit) != 0)
+    {
+        // supports AVX512
+        return 1;
     }
 
-    // does not support AVX2
+    // does not support AVX512
     return 0;
 }
--- runtimechecks/check_avx_support.cpp
+++ runtimechecks/check_avx_support.cpp
@@ -1,14 +1,43 @@
+#include <stdio.h>
+
+#if defined(_MSC_VER)
+#include <intrin.h>
+
+void cpuid(int cpuInfo[4], int function_id) {
+    __cpuid(cpuInfo, function_id);
+}
+
+void cpuidex(int cpuInfo[4], int function_id, int subfunction_id) {
+    __cpuidex(cpuInfo, function_id, subfunction_id);
+}
+
+#elif defined(__GNUC__) || defined(__clang__)
 #include <cpuid.h>
+
+void cpuid(int cpuInfo[4], int function_id) {
+    __get_cpuid(function_id, (unsigned int*)&cpuInfo[0], (unsigned int*)&cpuInfo[1], (unsigned int*)&cpuInfo[2], (unsigned int*)&cpuInfo[3]);
+}
+
+void cpuidex(int cpuInfo[4], int function_id, int subfunction_id) {
+    __cpuid_count(function_id, subfunction_id, cpuInfo[0], cpuInfo[1], cpuInfo[2], cpuInfo[3]);
+}
+
+#else
+#error "Unsupported compiler"
+#endif
 
 int main()
 {
-    unsigned int eax, ebx, ecx, edx;
-    if (__get_cpuid_count(1, 0, &eax, &ebx, &ecx, &edx)) {
-        if ((ecx & bit_AVX) != 0)
-        {
-            // supports AVX
-            return 1;
-        }
+    int cpuInfo[4];
+
+    cpuid(cpuInfo, 1);
+    int ecx = cpuInfo[2];
+    int bitAvx = 1 << 28;
+
+    if ((ecx & bitAvx) != 0)
+    {
+        // supports AVX
+        return 1;
     }
 
     // does not support AVX
