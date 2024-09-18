074b31ba6f2293c44f909b1684477c46c7f89f74
VMikulasek <mikulasekvitek@gmail.com>
2024-09-18 22:39:24 +0200

    feat: wrap necessary int AVX intrinsics to implement vectorized fast inverse square root

--- include/simd/detail/operations/simd_operations_avx2_int.hpp
+++ include/simd/detail/operations/simd_operations_avx2_int.hpp
@@ -23,12 +23,72 @@ namespace simd
          * 
          * @param arr Int array to load to register,
          * 8 ints will be loaded to register, so has to be
-         * at least 8 ints long, also has to be aligned to 32 bits
+         * at least 8 ints long, also has to be aligned to 32 bytes
          * otherwise segmentation fault will be generated
          * 
          * @return Register with loaded vector
          */
         static Avx2IReg load_vector(const int *arr);
+
+        /**
+         * @brief Loads zeros to register which is returned
+         * 
+         * @return Register with zeros
+         */
+        static Avx2IReg set_register_zero();
+
+        /**
+         * @brief Sets every field of register to num
+         */
+        static Avx2IReg set_register(int num);
+
+        /**
+         * @brief Stores vector from register to int *dst
+         * 
+         * @param reg register to materialize
+         * @param dst int * aligned to 32 bytes with at least 8 ints
+         * of size
+         */
+        static void materialize_register(Avx2IReg &reg, int *dst);
+
+        /**
+         * @brief Converts register of floats to register of ints
+         * 
+         * @return Avx2IReg register of ints
+         */
+        static Avx2IReg convert_from_float(__m256 &floatReg);
+
+        /**
+         * @brief Adds vec2 to vec1 and returns result
+         */
+        static Avx2IReg add(Avx2IReg &vec1, Avx2IReg &vec2);
+
+        /**
+         * @brief Subtracts vec2 from vec1 and returns result
+         */
+        static Avx2IReg sub(Avx2IReg &vec1, Avx2IReg &vec2);
+
+        /**
+         * @brief Shift every int in reg left by count bits
+         * while shifting in zeros
+         * 
+         * @param reg Register to shift
+         * @param count Count of bits to shift, if more than 32,
+         * then undefined behavior
+         * @return Avx2IReg shifted result
+         */
+        static Avx2IReg shift_left(Avx2IReg &vec, int count);
+
+        /**
+         * @brief Shift every int in reg right by count bits
+         * while shifting in zeros
+         * 
+         * @param reg Register to shift
+         * @param count Count of bits to shift, if more than 32,
+         * then undefined behavior
+         * @return Avx2IReg Shifted result
+         */
+        static Avx2IReg shift_right(Avx2IReg &vec, int count);
     };
 } // namespace simd
 
--- include/simd/detail/operations/simd_operations_avx2_int.inl
+++ include/simd/detail/operations/simd_operations_avx2_int.inl
@@ -8,7 +8,54 @@ namespace simd
     inline SIMDOperations<int, InstructionSet::AVX2>::Avx2IReg SIMDOperations<int, InstructionSet::AVX2>::load_vector(
         const int *arr)
     {
-        return _mm256_load_si256(reinterpret_cast<const __m256i *>(arr));
+        return _mm256_load_si256(reinterpret_cast<const Avx2IReg *>(arr));
+    }
+
+    inline SIMDOperations<int, InstructionSet::AVX2>::Avx2IReg SIMDOperations<int, InstructionSet::AVX2>::set_register_zero()
+    {
+        return _mm256_setzero_si256();
+    }
+
+    inline SIMDOperations<int, InstructionSet::AVX2>::Avx2IReg SIMDOperations<int, InstructionSet::AVX2>::set_register(
+        int num)
+    {
+        return _mm256_set1_epi32(num);
+    }
+
+    inline void SIMDOperations<int, InstructionSet::AVX2>::materialize_register(
+        Avx2IReg &reg, int *dst)
+    {
+        _mm256_store_si256(reinterpret_cast<Avx2IReg *>(dst), reg);
+    }
+
+    inline SIMDOperations<int, InstructionSet::AVX2>::Avx2IReg SIMDOperations<int, InstructionSet::AVX2>::convert_from_float(
+        __m256 &floatReg)
+    {
+        return _mm256_cvtps_epi32(floatReg);
+    }
+
+    inline SIMDOperations<int, InstructionSet::AVX2>::Avx2IReg SIMDOperations<int, InstructionSet::AVX2>::add(
+        Avx2IReg &vec1, Avx2IReg &vec2)
+    {
+        return _mm256_add_epi32(vec1, vec2);
+    }
+
+    inline SIMDOperations<int, InstructionSet::AVX2>::Avx2IReg SIMDOperations<int, InstructionSet::AVX2>::sub(
+        Avx2IReg &vec1, Avx2IReg &vec2)
+    {
+        return _mm256_sub_epi32(vec1, vec2);
+    }
+
+    inline SIMDOperations<int, InstructionSet::AVX2>::Avx2IReg SIMDOperations<int, InstructionSet::AVX2>::shift_left(
+        Avx2IReg &vec, int count)
+    {
+        return _mm256_slli_epi32(vec, count);
+    }
+
+    inline SIMDOperations<int, InstructionSet::AVX2>::Avx2IReg SIMDOperations<int, InstructionSet::AVX2>::shift_right(
+        Avx2IReg &vec, int count)
+    {
+        return _mm256_srli_epi32(vec, count);
     }
 } // namespace simd
 
