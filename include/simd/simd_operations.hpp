869897da1dfc25787d6f43e57bef6f65838018fe
VMikulasek <mikulasekvitek@gmail.com>
2024-09-18 22:54:17 +0200

    feat: include avx2 intrinsics into main headers

--- include/simd/simd_operations.hpp
+++ include/simd/simd_operations.hpp
@@ -4,5 +4,8 @@
 #ifdef HAS_AVX
 #include <simd/simd_operations_avx.hpp>
 #endif
+#ifdef HAS_AVX2
+#include <simd/simd_operations_avx2.hpp>
+#endif
 
 #endif // SIMD_OPERATIONS_HPP
---
+++ include/simd/simd_operations_avx2.hpp
@@ -0,0 +1,6 @@
+#ifndef SIMD_OPERATIONS_AVX2_HPP
+#define SIMD_OPERATIONS_AVX2_HPP
+
+#include <simd/detail/operations/simd_operations_avx2_int.hpp>
+
+#endif // SIMD_OPERATIONS_AVX2_HPP
