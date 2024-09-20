cd746947e94d1083b02852cddedde9a3a9a13e4a
VMikulasek <mikulasekvitek@gmail.com>
2024-09-20 19:16:04 +0200

    feat: change signature of prefix_sum + overload it with vector in parameters

--- include/mathops/detail/sums.inl
+++ include/mathops/detail/sums.inl
@@ -5,6 +5,8 @@
 #include <mathops/detail/AVX/avx_sums.hpp>
 #include <mathops/detail/SEQ/seq_sums.hpp>
 
+#include <mathops/detail/common/memory_common.inl>
+
 namespace mathops
 {
 
@@ -25,9 +27,20 @@ namespace mathops
         return SUM(arr, size);
     }
 
-    inline void prefix_sum(const float *arr, size_t size, float *dstArr)
+    inline float *prefix_sum(const float *arr, size_t size)
     {
-        PREFIX_SUM(arr, size, dstArr);
+        float *dst = _alloc_aligned_memory_float(size * sizeof(float), AVX_ALIGNMENT);
+        if (dst == nullptr)
+        {
+            return nullptr;
+        }
+
+        PREFIX_SUM(arr, size, dst);
+    }
+
+    inline float *prefix_sum(const std::vector<float> &arr)
+    {
+        return prefix_sum(arr.data(), arr.size());
     }
 } // namespace math
 
--- include/mathops/sums.hpp
+++ include/mathops/sums.hpp
@@ -2,6 +2,7 @@
 #define SUMS_HPP
 
 #include <cstddef>
+#include <vector>
 
 namespace mathops
 {
@@ -14,7 +15,8 @@ namespace mathops
      * @param arr Array to sum, aligned to 32 bytes,
      * otherwise segmentation fault may be generated
      * @param size Size of array
-     * @return float Sum of arr
+     * 
+     * @returns float Sum of arr
      */
     float sum(const float *arr, size_t size);
     
@@ -27,11 +29,29 @@ namespace mathops
      * @param arr Array to perform prefix sum on,
      * has to be aligned to 32 bytes
      * @param size Size of array
-     * @param dstArr Array to store result to, must be
-     * at least the size of arr, has to be aligned to
-     * 32 bytes
+     * 
+     * @returns Array with results and size of size floats,
+     * that has to be freed after use.
+     * If used on windows, then has to be freed with _aligned_free().
+     * In case of allocation fail returns nullptr.
      */
-    void prefix_sum(const float *arr, size_t size, float *dstArr);
+    float *prefix_sum(const float *arr, size_t size);
+
+    /**
+     * @brief Computes prefix sum of arr accelerated
+     * with biggest SIMD unit supported and available on
+     * PC, in case of no SIMD unit available, computes
+     * sequentially 
+     * 
+     * @param arr Vector to perform prefix sum on,
+     * vector.data() has to be aligned to 32 bytes
+     * 
+     * @returns Array with results and size of size vector.size() floats,
+     * that has to be freed after use.
+     * If used on windows, then has to be freed with _aligned_free().
+     * In case of allocation fail returns nullptr.
+     */
+    float *prefix_sum(const std::vector<float> &arr);
 } // namespace math
 
 #include <mathops/detail/sums.inl>
