d7665dc02fe051a071b1b4089ee4dd9cec9bcd62
VMikulasek <mikulasekvitek@gmail.com>
2024-10-01 21:46:57 +0200

    feat: implement sqrt and invsqrt arr tests

--- analysis/tests/mathops/AVX/avx_transcedentals_tests.cpp
+++ analysis/tests/mathops/AVX/avx_transcedentals_tests.cpp
@@ -1,5 +1,5 @@
 #include <mathops/transcedentals_tests_fixture.hpp>
-#include <mathops/detail/SEQ/seq_transcedentals.hpp>
+#include <mathops/detail/AVX/avx_transcedentals.hpp>
 #include <mathops_shared_fields.hpp>
 
 #include <gtest/gtest.h>
@@ -9,84 +9,84 @@ namespace analysis
 {
 namespace tests
 {
-    class SeqTranscedentalsTests : public TranscedentalsTests
+    class AvxTranscedentalsTests : public TranscedentalsTests
     {
     protected:
         void test_sqrt_arr(const float *arr, size_t size)
         {
-            TranscedentalsTests::test_sqrt_arr(mathops::seq::fast_sqrt_arr,
+            TranscedentalsTests::test_sqrt_arr(mathops::avx::fast_sqrt_arr,
                 arr, size);
         }
 
         void test_invsqrt_arr(const float *arr, size_t size)
         {
-            TranscedentalsTests::test_invsqrt_arr(mathops::seq::fast_invsqrt_arr,
+            TranscedentalsTests::test_invsqrt_arr(mathops::avx::fast_invsqrt_arr,
                 arr, size);
         }
     };
 
-    TEST_F(SeqTranscedentalsTests, SqrtArr1ElemArr)
+    TEST_F(AvxTranscedentalsTests, SqrtArr1ElemArr)
     {
         test_sqrt_arr(_1ElemArr, _1_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, SqrtArr8ElemArr)
+    TEST_F(AvxTranscedentalsTests, SqrtArr8ElemArr)
     {
         test_sqrt_arr(_8ElemArr, _8_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, SqrtArr9ElemArr)
+    TEST_F(AvxTranscedentalsTests, SqrtArr9ElemArr)
     {
         test_sqrt_arr(_9ElemArr, _9_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, SqrtArr16ElemArr)
+    TEST_F(AvxTranscedentalsTests, SqrtArr16ElemArr)
     {
         test_sqrt_arr(_16ElemArr, _16_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, SqrtArr17ElemArr)
+    TEST_F(AvxTranscedentalsTests, SqrtArr17ElemArr)
     {
         test_sqrt_arr(_17ElemArr, _17_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, SqrtArr24ElemArr)
+    TEST_F(AvxTranscedentalsTests, SqrtArr24ElemArr)
     {
         test_sqrt_arr(_24ElemArr, _24_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, SqrtArr25ElemArr)
+    TEST_F(AvxTranscedentalsTests, SqrtArr25ElemArr)
     {
         test_sqrt_arr(_25ElemArr, _25_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, SqrtArrBigArr)
+    TEST_F(AvxTranscedentalsTests, SqrtArrBigArr)
     {
         test_sqrt_arr(_bigArr, BIG_ARR_SIZE);
     }
 
-    TEST_F(SeqTranscedentalsTests, InvSqrtArr1ElemArr)
+    TEST_F(AvxTranscedentalsTests, InvSqrtArr1ElemArr)
     {
         test_invsqrt_arr(_1ElemArr, _1_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, InvSqrtArr8ElemArr)
+    TEST_F(AvxTranscedentalsTests, InvSqrtArr8ElemArr)
     {
         test_invsqrt_arr(_8ElemArr, _8_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, InvSqrtArr9ElemArr)
+    TEST_F(AvxTranscedentalsTests, InvSqrtArr9ElemArr)
     {
         test_invsqrt_arr(_9ElemArr, _9_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, InvSqrtArr16ElemArr)
+    TEST_F(AvxTranscedentalsTests, InvSqrtArr16ElemArr)
     {
         test_invsqrt_arr(_16ElemArr, _16_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, InvSqrtArr17ElemArr)
+    TEST_F(AvxTranscedentalsTests, InvSqrtArr17ElemArr)
     {
         test_invsqrt_arr(_17ElemArr, _17_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, InvSqrtArr24ElemArr)
+    TEST_F(AvxTranscedentalsTests, InvSqrtArr24ElemArr)
     {
         test_invsqrt_arr(_24ElemArr, _24_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, InvSqrtArr25ElemArr)
+    TEST_F(AvxTranscedentalsTests, InvSqrtArr25ElemArr)
     {
         test_invsqrt_arr(_25ElemArr, _25_ELEM_ARR_SIZE);
     }
-    TEST_F(SeqTranscedentalsTests, InvSqrtArrBigArr)
+    TEST_F(AvxTranscedentalsTests, InvSqrtArrBigArr)
     {
         test_invsqrt_arr(_bigArr, BIG_ARR_SIZE);
     }
