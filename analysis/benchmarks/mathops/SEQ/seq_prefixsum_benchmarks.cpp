37dca7beb27d4c1c31685ad14e8e0b593689a335
VMikulasek <mikulasekvitek@gmail.com>
2024-10-02 00:05:31 +0200

    refactor: remake structure for benchmarks

--- analysis/benchmarks/mathops/SEQ/seq_prefixsum_benchmarks.cpp
+++ analysis/benchmarks/mathops/SEQ/seq_prefixsum_benchmarks.cpp
@@ -1,6 +1,6 @@
 #include <mathops_shared_fields.hpp>
+#include <mathops/detail/SEQ/seq_sums.hpp>
 #include <common/memory_common.inl>
-#include <mathops/detail/AVX/avx_sums.hpp>
 
 #include <benchmark/benchmark.h>
 
@@ -8,67 +8,13 @@ namespace analysis
 {
 namespace benchmarks
 {
-    static void BM_Sum(benchmark::State &state, const float *arr, size_t size)
-    {
-        for (auto _ : state)
-        {
-            mathops::avx::sum(arr, size);
-        }
-    }
-
-    static void BM_Sum1Elem(benchmark::State &state)
-    {
-        BM_Sum(state, _1ElemArr, _1_ELEM_ARR_SIZE);
-    }
-    static void BM_Sum8Elem(benchmark::State &state)
-    {
-        BM_Sum(state, _8ElemArr, _8_ELEM_ARR_SIZE);
-    }
-    static void BM_Sum9Elem(benchmark::State &state)
-    {
-        BM_Sum(state, _9ElemArr, _9_ELEM_ARR_SIZE);
-    }
-    static void BM_Sum16Elem(benchmark::State &state)
-    {
-        BM_Sum(state, _16ElemArr, _16_ELEM_ARR_SIZE);
-    }
-    static void BM_Sum17Elem(benchmark::State &state)
-    {
-        BM_Sum(state, _17ElemArr, _17_ELEM_ARR_SIZE);
-    }
-    static void BM_Sum24Elem(benchmark::State &state)
-    {
-        BM_Sum(state, _24ElemArr, _24_ELEM_ARR_SIZE);
-    }
-    static void BM_Sum25Elem(benchmark::State &state)
-    {
-        BM_Sum(state, _25ElemArr, _25_ELEM_ARR_SIZE);
-    }
-    static void BM_SumBigArr(benchmark::State &state)
-    {
-        float *bigArr = AllocBigArr();
-
-        BM_Sum(state, bigArr, BIG_ARR_SIZE);
-
-        _free_aligned_memory(bigArr);
-    }
-
-    BENCHMARK(BM_Sum1Elem);
-    BENCHMARK(BM_Sum8Elem);
-    BENCHMARK(BM_Sum9Elem);
-    BENCHMARK(BM_Sum16Elem);
-    BENCHMARK(BM_Sum17Elem);
-    BENCHMARK(BM_Sum24Elem);
-    BENCHMARK(BM_Sum25Elem);
-    BENCHMARK(BM_SumBigArr);
-
     static void BM_PrefixSum(benchmark::State &state, const float *srcArr, size_t size)
     {
         float *dst = _alloc_aligned_memory_float(size * sizeof(float), AVX_ALIGNMENT);
 
         for (auto _ : state)
         {
-            mathops::avx::prefix_sum(srcArr, size, dst);
+            mathops::seq::prefix_sum(srcArr, size, dst);
         } 
 
         _free_aligned_memory(dst);
@@ -119,5 +65,5 @@ namespace benchmarks
     BENCHMARK(BM_PrefixSum24Elem);
     BENCHMARK(BM_PrefixSum25Elem);
     BENCHMARK(BM_PrefixSumBigArr);
-} // namespace benchmarks
-} // namespace analysis
+}
+}
---
+++ analysis/benchmarks/mathops/SEQ/seq_sum_benchmarks.cpp
@@ -0,0 +1,64 @@
+#include <mathops_shared_fields.hpp>
+#include <mathops/detail/SEQ/seq_sums.hpp>
+
+#include <benchmark/benchmark.h>
+
+namespace analysis
+{
+namespace benchmarks
+{
+    static void BM_Sum(benchmark::State &state, const float *arr, size_t size)
+    {
+        for (auto _ : state)
+        {
+            mathops::seq::sum(arr, size);
+        }
+    }
+
+    static void BM_Sum1Elem(benchmark::State &state)
+    {
+        BM_Sum(state, _1ElemArr, _1_ELEM_ARR_SIZE);
+    }
+    static void BM_Sum8Elem(benchmark::State &state)
+    {
+        BM_Sum(state, _8ElemArr, _8_ELEM_ARR_SIZE);
+    }
+    static void BM_Sum9Elem(benchmark::State &state)
+    {
+        BM_Sum(state, _9ElemArr, _9_ELEM_ARR_SIZE);
+    }
+    static void BM_Sum16Elem(benchmark::State &state)
+    {
+        BM_Sum(state, _16ElemArr, _16_ELEM_ARR_SIZE);
+    }
+    static void BM_Sum17Elem(benchmark::State &state)
+    {
+        BM_Sum(state, _17ElemArr, _17_ELEM_ARR_SIZE);
+    }
+    static void BM_Sum24Elem(benchmark::State &state)
+    {
+        BM_Sum(state, _24ElemArr, _24_ELEM_ARR_SIZE);
+    }
+    static void BM_Sum25Elem(benchmark::State &state)
+    {
+        BM_Sum(state, _25ElemArr, _25_ELEM_ARR_SIZE);
+    }
+    static void BM_SumBigArr(benchmark::State &state)
+    {
+        float *bigArr = AllocBigArr();
+
+        BM_Sum(state, bigArr, BIG_ARR_SIZE);
+
+        _free_aligned_memory(bigArr);
+    }
+
+    BENCHMARK(BM_Sum1Elem);
+    BENCHMARK(BM_Sum8Elem);
+    BENCHMARK(BM_Sum9Elem);
+    BENCHMARK(BM_Sum16Elem);
+    BENCHMARK(BM_Sum17Elem);
+    BENCHMARK(BM_Sum24Elem);
+    BENCHMARK(BM_Sum25Elem);
+    BENCHMARK(BM_SumBigArr);
+} // namespace benchmarks
+} // namespace analysis
