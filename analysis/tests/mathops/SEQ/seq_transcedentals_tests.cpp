6fede9d72496eb9bdfa2e3e640ddd5bfe8f7c77b
VMikulasek <mikulasekvitek@gmail.com>
2024-10-28 23:05:57 +0100

    feat: add fast_sin_arr fast_cos_arr seq version tests

--- analysis/tests/mathops/SEQ/seq_transcedentals_tests.cpp
+++ analysis/tests/mathops/SEQ/seq_transcedentals_tests.cpp
@@ -23,6 +23,18 @@ namespace tests
             TranscedentalsTests::test_invsqrt_arr(mathops::seq::fast_invsqrt_arr,
                 arr, size);
         }
+
+        void test_sin_arr(const float *arr, size_t size)
+        {
+            TranscedentalsTests::test_sin_arr(mathops::seq::fast_sin_arr,
+                arr, size);
+        }
+
+        void test_cos_arr(const float *arr, size_t size)
+        {
+            TranscedentalsTests::test_cos_arr(mathops::seq::fast_cos_arr,
+                arr, size);
+        }
     };
 
     TEST_F(SeqTranscedentalsTests, SqrtArr1ElemArr)
@@ -89,6 +101,72 @@ namespace tests
     TEST_F(SeqTranscedentalsTests, InvSqrtArrBigArr)
     {
         test_invsqrt_arr(_bigArr, BIG_ARR_SIZE);
+    }
+
+    TEST_F(SeqTranscedentalsTests, SinArr1ElemArr)
+    {
+        test_sin_arr(_1ElemArr, _1_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, SinArr8ElemArr)
+    {
+        test_sin_arr(_8ElemArrMinusPiPi, _8_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, SinArr9ElemArr)
+    {
+        test_sin_arr(_9ElemArrMinusPiPi, _9_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, SinArr16ElemArr)
+    {
+        test_sin_arr(_16ElemArrMinusPiPi, _16_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, SinArr17ElemArr)
+    {
+        test_sin_arr(_17ElemArrMinusPiPi, _17_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, SinArr24ElemArr)
+    {
+        test_sin_arr(_24ElemArrMinusPiPi, _24_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, SinArr25ElemArr)
+    {
+        test_sin_arr(_25ElemArrMinusPiPi, _25_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, SinArrBigArr)
+    {
+        test_sin_arr(_bigArr, BIG_ARR_SIZE);
+    }
+
+    TEST_F(SeqTranscedentalsTests, CosArr1ElemArr)
+    {
+        test_cos_arr(_1ElemArr, _1_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, CosArr8ElemArr)
+    {
+        test_cos_arr(_8ElemArrMinusPiPi, _8_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, CosArr9ElemArr)
+    {
+        test_cos_arr(_9ElemArrMinusPiPi, _9_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, CosArr16ElemArr)
+    {
+        test_cos_arr(_16ElemArrMinusPiPi, _16_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, CosArr17ElemArr)
+    {
+        test_cos_arr(_17ElemArrMinusPiPi, _17_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, CosArr24ElemArr)
+    {
+        test_cos_arr(_24ElemArrMinusPiPi, _24_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, CosArr25ElemArr)
+    {
+        test_cos_arr(_25ElemArrMinusPiPi, _25_ELEM_ARR_SIZE);
+    }
+    TEST_F(SeqTranscedentalsTests, CosArrBigArr)
+    {
+        test_cos_arr(_bigArr, BIG_ARR_SIZE);
     }
 } // namespace tests
 } // namespace analysis
