e8666b9f4842ca5c8afc1bbb9bd856c5a7350357
VMikulasek <mikulasekvitek@gmail.com>
2025-02-01 22:58:21 +0100

    feat: add Vec2 structure

---
+++ include/simd/detail/vec/simd_vec2.hpp
@@ -0,0 +1,36 @@
+#ifndef SIMD_VEC2_HPP
+#define SIMD_VEC2_HPP
+
+#include <simd/detail/vec/simd_vec_t.hpp>
+
+namespace simd
+{
+    template<typename T>
+    class Vec<2, T>
+    {
+    public:
+        union
+        {
+            struct{ T x, y; };
+            T data[2];
+        };
+        
+        float data[2];
+
+        inline Vec(T x, T y);
+        inline Vec();
+
+        inline Vec operator+(const Vec &other) const;
+        inline Vec operator-(const Vec &other) const;
+        inline Vec operator*(const Vec &other) const;
+        inline Vec operator/(const Vec &other) const;
+        inline static float dot(const Vec &vec1, const Vec &vec2);
+    };
+
+    using Vec2f = Vec<2, float>;
+    using Vec2 = Vec<2, int>;
+} // namespace simd
+
+#include <simd/detail/vec/simd_vec2.inl>
+
+#endif
---
+++ include/simd/detail/vec/simd_vec2.inl
@@ -0,0 +1,6 @@
+#ifndef SIMD_VEC2_INL
+#define SIMD_VEC2_INL
+
+#include <simd/detail/vec/simd_vec2.hpp>
+
+#endif
