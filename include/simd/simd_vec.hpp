e2c0e9cdf29e2d28b5a91ad50885adaa9b15d3c0
VMikulasek <mikulasekvitek@gmail.com>
2025-02-06 01:31:19 +0100

    feat: update structure to new files

--- include/simd/simd_vec.hpp
+++ include/simd/simd_vec.hpp
@@ -2,5 +2,8 @@
 #define SIMDVEC_HPP
 
 #include <simd/detail/vec/simd_vec_t.hpp>
+#include <simd/detail/vec/simd_vec2.hpp>
+#include <simd/detail/vec/simd_vec3.hpp>
+#include <simd/detail/vec/simd_vec4.hpp>
 
 #endif // SIMDVEC_HPP
