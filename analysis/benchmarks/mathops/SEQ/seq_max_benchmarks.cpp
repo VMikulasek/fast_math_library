8ec3ab2ab6a5c4d3cb47615c8031576c61c9f5d3
VMikulasek <mikulasekvitek@gmail.com>
2025-03-02 13:06:35 +0100

    feat: add avx min and seq, avx max benchmarks

---
+++ analysis/benchmarks/mathops/SEQ/seq_max_benchmarks.cpp
@@ -0,0 +1,45 @@
+#include <mathops_shared_fields.hpp>
+#include <common/detail/memory_common.inl>
+#include <mathops/detail/SEQ/seq_statisticals.hpp>
+
+#include <benchmark/benchmark.h>
+
+namespace analysis
+{
+namespace benchmarks
+{
+    static void BM_Max(benchmark::State &state, const float *arr, size_t size)
+    {
+        for (auto _ : state)
+        {
+            float res = mathops::seq::max(arr, size);
+            benchmark::DoNotOptimize(res);
+        }
+    }
+
+    static void BM_Max9Elem(benchmark::State &state)
+    {
+        BM_Max(state, _9ElemArr, _9_ELEM_ARR_SIZE);
+    }
+    static void BM_Max10kElem(benchmark::State &state)
+    {
+        float *medArr = AllocMediumArr();
+
+        BM_Max(state, medArr, MEDIUM_ARR_SIZE);
+
+        _free_aligned_memory(medArr);
+    }
+    static void BM_Max15MElem(benchmark::State &state)
+    {
+        float *bigArr = AllocBigArr();
+
+        BM_Max(state, bigArr, BIG_ARR_SIZE);
+
+        _free_aligned_memory(bigArr);
+    }
+
+    BENCHMARK(BM_Max9Elem);
+    BENCHMARK(BM_Max10kElem);
+    BENCHMARK(BM_Max15MElem);
+} // namespace benchmarks
+} // namespace analysis
