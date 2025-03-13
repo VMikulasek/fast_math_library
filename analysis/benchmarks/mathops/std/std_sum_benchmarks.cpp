951653c566b0eb702effae5efe5a5d099446bdb6
VMikulasek <mikulasekvitek@gmail.com>
2025-03-13 18:20:11 +0100

    feat: add std benchmarks

---
+++ analysis/benchmarks/mathops/std/std_sum_benchmarks.cpp
@@ -0,0 +1,51 @@
+#include <mathops_shared_fields.hpp>
+
+#include <numeric>
+#include <execution>
+
+#include <benchmark/benchmark.h>
+
+namespace analysis
+{
+namespace benchmarks
+{
+#ifdef HAS_AVX
+
+    static void BM_Sum(benchmark::State &state, const float *arr, size_t size)
+    {
+        for (auto _ : state)
+        {
+            benchmark::DoNotOptimize(arr);
+            float res = std::reduce(std::execution::par, arr, arr + size);
+            benchmark::DoNotOptimize(res);
+        }
+    }
+
+    static void BM_Sum9Elem(benchmark::State &state)
+    {
+        BM_Sum(state, _9ElemArr, _9_ELEM_ARR_SIZE);
+    }
+    static void BM_Sum10kElem(benchmark::State &state)
+    {
+        float *medArr = AllocMediumArr();
+
+        BM_Sum(state, medArr, MEDIUM_ARR_SIZE);
+
+        _free_aligned_memory(medArr);
+    }
+    static void BM_Sum15MElem(benchmark::State &state)
+    {
+        float *bigArr = AllocBigArr();
+
+        BM_Sum(state, bigArr, BIG_ARR_SIZE);
+
+        _free_aligned_memory(bigArr);
+    }
+
+    BENCHMARK(BM_Sum9Elem);
+    BENCHMARK(BM_Sum10kElem);
+    BENCHMARK(BM_Sum15MElem);
+
+#endif // HAS_AVX
+} // namespace benchmarks
+} // namespace analysis
