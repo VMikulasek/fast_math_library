8f0584f08fbb09d0bafe55cb667caa2949b389df
VMikulasek <mikulasekvitek@gmail.com>
2025-03-13 19:05:05 +0100

    feat: add std min, max benchmarks

---
+++ analysis/benchmarks/mathops/std/std_min_benchmarks.cpp
@@ -0,0 +1,51 @@
+#include <mathops_shared_fields.hpp>
+
+#include <algorithm>
+#include <execution>
+
+#include <benchmark/benchmark.h>
+
+namespace analysis
+{
+namespace benchmarks
+{
+#ifdef HAS_AVX
+
+    static void BM_Min(benchmark::State &state, const float *arr, size_t size)
+    {
+        for (auto _ : state)
+        {
+            benchmark::DoNotOptimize(arr);
+            auto res = std::min_element(std::execution::unseq, arr, arr + size);
+            benchmark::DoNotOptimize(res);
+        }
+    }
+
+    static void BM_Min9Elem(benchmark::State &state)
+    {
+        BM_Min(state, _9ElemArr, _9_ELEM_ARR_SIZE);
+    }
+    static void BM_Min10kElem(benchmark::State &state)
+    {
+        float *medArr = AllocMediumArr();
+
+        BM_Min(state, medArr, MEDIUM_ARR_SIZE);
+
+        _free_aligned_memory(medArr);
+    }
+    static void BM_Min15MElem(benchmark::State &state)
+    {
+        float *bigArr = AllocBigArr();
+
+        BM_Min(state, bigArr, BIG_ARR_SIZE);
+
+        _free_aligned_memory(bigArr);
+    }
+
+    BENCHMARK(BM_Min9Elem);
+    BENCHMARK(BM_Min10kElem);
+    BENCHMARK(BM_Min15MElem);
+
+#endif // HAS_AVX
+} // namespace benchmarks
+} // namespace analysis
